996bb3843553d97fcb20bc88378d52a5544b5f75
Mohammed Umar <mohammed60413@gmail.com>
2024-11-21 17:11:52 +0530

    Update quran.py

--- quran.py
+++ quran.py
@@ -3,14 +3,14 @@ st.write('''From Umar \n
 Contact: mohammedumar60413@outlook.com''')
 st.markdown("<h1 style='text-align: center;'> Islamic Resource Center </h1>", unsafe_allow_html=True)
 
-st.title("View & Read the Quran")
+st.title("Quran MP3 Files")
 
-# URL of the hosted webpage
-webpage_url = "https://www.islamicnet.com/quran.php"
+# URL of the website to embed
+webpage_urly = 'https://quranicaudio.com/'
+# Embed the webpage using iframe
+st.markdown(f'<iframe src="{webpage_urly}" width="800" height="600"></iframe>', unsafe_allow_html=True)
 
-# Embed the webpage using the iframe HTML tag
-st.markdown(f'<iframe src="{webpage_url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
-st.title('Full Quran Recitation Videos')
+
 st.title('Full Quran Recitation Videos')
 st.write("[Surah No 1 : Surah Fathihah - By Sheikh Abdur Rahman As Sudais](https://youtu.be/u1nqafqyqTA?feature=shared)")
 st.write("[Surah No 2 : Surah Al Baqrah - By Sheikh Abdur Rahman As Sudais](https://youtu.be/nibOG3vRGpU?feature=shared)")
