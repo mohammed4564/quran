005bc5493877a5886233237be78162ec40f4761b
Mohammed Umar <mohammed60413@gmail.com>
2024-11-21 17:42:04 +0530

    Update quran.py

--- quran.py
+++ quran.py
@@ -3,12 +3,6 @@ st.write('''From Umar \n
 Contact: mohammedumar60413@outlook.com''')
 
 
-st.title("View & Read the Quran PDF")
-pdf_url = "https://www.islamicnet.com/quran.php"
-st.markdown(f'<object data="{pdf_url}" type="application/pdf" width="700" height="900"></object>', unsafe_allow_html=True)
-
-st.write("===============================================================================================================")
-
 st.title("Quran MP3 Files")
 webpage_urly = 'https://quranicaudio.com/'
 st.markdown(f'<iframe src="{webpage_urly}" width="800" height="600"></iframe>', unsafe_allow_html=True)
@@ -130,5 +124,7 @@ st.write("[Surah No :112 - Surah Iqlas - By Sheikh Abdur Rahman As Sudais](https
 st.write("[Surah No :113 - Surah Falak - By Sheikh Abdur Rahman As Sudais](https://youtu.be/gUtD5O5AmaQ?feature=shared)")
 st.write("[Surah No :114 - Surah An Naas - By Sheikh Abdur Rahman As Sudais](https://youtu.be/ZIUSO7aAJPY?feature=shared)")
 
-
+st.title("View & Read the Quran PDF")
+pdf_url = "https://www.islamicnet.com/quran.php"
+st.markdown(f'<object data="{pdf_url}" type="application/pdf" width="700" height="900"></object>', unsafe_allow_html=True)
 
