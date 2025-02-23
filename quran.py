4c6187b31e2012bc997837db57305b69eddbcec8
Mohammed Umar <mohammed60413@gmail.com>
2025-02-23 14:40:23 +0530

    Update quran.py

--- quran.py
+++ quran.py
@@ -94,7 +94,7 @@ st.markdown("""
             padding: 12px;
             text-align: center;
             border: 1px solid #ddd;
-            min-width: 260px;  /* Ensure columns have a fixed minimum width */
+            min-width: 240px;  /* Ensure columns have a fixed minimum width */
         }
         th {
             background-color: #f4f4f4;
